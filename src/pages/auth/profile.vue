0d372a4bc0e4fe05be40c18169029bc58aed69c8
kromate <akpananthony33@gmail.com>
2024-09-08 18:36:38 +0100

    save_paoint

--- src/pages/auth/profile.vue
+++ src/pages/auth/profile.vue
@@ -6,30 +6,29 @@
 		</h1>
 
 		<form class="auth-form " @submit.prevent="createProfile">
-			<div id="step 1" class="auth-form">
-				<div class="field">
-					<label for="last_name">Full Name</label>
-					<input id="last_name" v-model="profileFormState.name.value" type="text" class="input-field" required>
-				</div>
-				<div class="field relative">
-					<label for="username">Username</label>
-					<input id="username" v-model="profileFormState.username.value" type="text" class="input-field" autocomplete="additional-name2" :class="[isUsernameAvailable ? '' : '!border-rose-500']" required>
-					<Spinner v-if="usernameLoading" class="!border-t-dark !border-[#0c030366] absolute right-4 top-9" />
-					<span v-if="!isUsernameAvailable" class="text-rose-500 font-bold">This username is taken</span>
-				</div>
-
-
-				<div class="field">
-					<label for="email">Email</label>
-					<input id="email" v-model="profileFormState.email.value" :disabled="!!user!.email" type="text" class="input-field" required>
-				</div>
-
-				<div class="field">
-					<PhoneInput v-model="profileFormState.phone.value" :class="[phoneNumError ? '!border-rose-500' : '']" label="Phone No (whatsapp preferred)" :disabled="!!user!.phoneNumber" />
-
-					<span v-if="phoneNumError" class="text-rose-500 font-bold">{{ phoneNumError }}</span>
-				</div>
+			<div class="field">
+				<label for="last_name">Full Name</label>
+				<input id="last_name" v-model="profileFormState.name.value" type="text" class="input-field" required>
 			</div>
+			<div class="field relative">
+				<label for="username">Username</label>
+				<input id="username" v-model="profileFormState.username.value" type="text" class="input-field" autocomplete="additional-name2" :class="[isUsernameAvailable ? '' : '!border-rose-500']" required>
+				<Spinner v-if="usernameLoading" class="!border-t-dark !border-[#0c030366] absolute right-4 top-9" />
+				<span v-if="!isUsernameAvailable" class="text-rose-500 font-bold">This username is taken</span>
+			</div>
+
+
+			<div class="field">
+				<label for="email">Email</label>
+				<input id="email" v-model="profileFormState.email.value" :disabled="!!user!.email" type="text" class="input-field" required>
+			</div>
+
+			<div class="field">
+				<PhoneInput v-model="profileFormState.phone.value" :class="[phoneNumError ? '!border-rose-500' : '']" label="Phone No (whatsapp preferred)" :disabled="!!user!.phoneNumber" />
+
+				<span v-if="phoneNumError" class="text-rose-500 font-bold">{{ phoneNumError }}</span>
+			</div>
+
 
 
 			<button class="btn-primary w-full mt-4" :disabled="disabled">
